01327f36b7131e11f2abc399f7539a80fb3a640a
Mattias Loverot <mattias@stubin.se>
2016-06-22 14:10:15 +0200

    Python 3 support

--- varnish.py
+++ varnish.py
@@ -33,11 +33,18 @@ https://www.varnish-cache.org/docs/3.0/tutorial/purging.html
 """
 from telnetlib import Telnet
 from threading import Thread
-from httplib import HTTPConnection
-from urlparse import urlparse
 from hashlib import sha256
 import logging
 
+try:
+    from httplib import HTTPConnection
+except ImportError:
+    from http.client import HTTPConnection  # py3
+
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse
 
 logging.basicConfig(
     level = logging.DEBUG,
@@ -61,7 +68,7 @@ def http_purge_url(url):
 
 class VarnishHandler(Telnet):
     def __init__(self, host_port_timeout, secret=None, **kwargs):
-        if isinstance(host_port_timeout, basestring):
+        if isinstance(host_port_timeout, str):
             host_port_timeout = host_port_timeout.split(':')
 
             if (len(host_port_timeout) == 3):
@@ -75,7 +82,7 @@ class VarnishHandler(Telnet):
             logging.error('Connecting failed with status: %i' % status)
 
     def _read(self):
-        (status, length), content = map(int, self.read_until('\n').split()), ''
+        (status, length), content = list(map(int, self.read_until('\n').split())), ''
         while len(content) < length:
             content += self.read_some()
         return (status, length), content[:-1]
@@ -91,7 +98,7 @@ class VarnishHandler(Telnet):
             buffer = self.read_until('\n').strip()
             if len(buffer):
                 break
-        status, length = map(int, buffer.split())
+        status, length = list(map(int, buffer.split()))
         content = ''
         assert status == 200, 'Bad response code: {status} {text} ({command})'.format(status=status, text=self.read_until('\n').strip(), command=command)
         while len(content) < length:
